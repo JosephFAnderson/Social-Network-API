c3c35ba24f59f777b05b93c4cfb5967a11863855
Joseph A <joseph_anderson11@yahoo.com>
2022-11-08 08:53:32 -0500

    create reaction route

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -4,11 +4,15 @@ const {
     getOneThought,
     createThought,
     updateThought,
-    deleteThought
+    deleteThought,
+    addReaction,
+
 } = require('../../controllers/thoughtController');
 
 router.route('/').get(getAllThoughts).post(createThought);
 
 router.route('/:id').get(getOneThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reaction').put(addReaction);
 
 module.exports = router;
